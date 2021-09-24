2d2db741209c99e54267f641dfade7111db4a432
yogesh salve <yogeshbalkrushnasalve@gmail.com>
2021-09-24 11:04:23 +0530

    24 sept 2021

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,7 +1,7 @@
 @extends('products.layout')
   
 @section('content')
-<div class="row">
+<!-- <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
             <h2>Add New Product</h2>
@@ -50,5 +50,5 @@
         </div>
     </div>
      
-</form>
+</form> -->
 @endsection
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -33,94 +33,11 @@
     <a href="" class="btn btn-primary">Upload Questions</a>
   <!-- </div> -->
 </div></div>
-
-<div class = "col-md-5  card">
-          <!-- first block start -->
-          <br>
-          <label>Exam :</label>
-          <select name="unreal">
-            <option style="display:none"></option>
-            <option>NEET</option>
-            <option>JEE</option>
-            <option>MHT-CET</option>
-          </select>    
-          <!-- first block end -->
-
-          <!-- second block start -->
-          <br>
-          <label>Select Standard : </label>
-          <select name="unreal">
-            <option style="display:none"></option>
-            <option>XI</option>
-            <option>XII</option>      
-          </select>  
-          <!-- second block end -->
-
-          <!-- third block starts -->
-          <br>
-          <label>Select Subject : </label>
-          <select name="unreal">
-            <option style="display:none"></option>
-            <option>XI</option>
-            <option>XII</option>      
-          </select>  
-          <!-- third block ends -->
-          <br>
-        </div>
-
-
-
-                    <div class = "col-md-5 card">
-
-  <div class="card-body">
-  <!-- <div class="alert alert-danger" role="alert">
-  A simple danger alert—check it out!
-</div> -->
-
-<!-- third card starts -->
-<!-- <div class="card">    
-  <div class ="card-body">
-
-  <form>
- 
-  <div class="form-group">
-    <label for="exampleFormControlFile1">Question</label>
-    <input type="file"  name= "question" class="form-control-file" id="exampleFormControlFile1">
+<div class ="card">
+<div class="card-header text-white bg-primary">
+    Upload New Question
   </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Answer</label>
-    <select class="form-control" name="answer" id="exampleFormControlSelect1">
-    <option selected disabled>Select Answer</option>
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-      <option>D</option>      
-    </select>
-  </div>
-  
-  <div class="form-group">
-    <label for="exampleFormControlFile1">Question Hint</label>
-    <input type="file" name="hint" class="form-control-file" id="exampleFormControlFile1">
-  </div>
-  <button type="submit" class="btn btn-primary mb-2">UPLOAD</button>
-</form>
-
-  </div>
-</div>
-</div> -->
-<!-- third card end -->
-
-<!-- <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add New Product</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
-        </div>
-    </div>
-</div> -->
-     
+<div class = "col-md-10">
 @if ($errors->any())
     <div class="alert alert-danger">
         <strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -136,24 +53,127 @@
         <div class="alert alert-success">
             <p>{{ $message }}</p>
         </div>
-    @endif
-     
-<form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
+    @endif 
+<br>
+<form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data"> 
+@csrf
+<div class="row justify-content-between g-3">
+  <div class="col-auto">  
+  <div class="btn-group btn-group-inline">
+  <div class="form-group">
+            <!-- <label for="exampleFormControlSelect1">Answer</label> -->
+            <select class="form-control" name="answer" id="exampleFormControlSelect1">
+            <option selected disabled>Select Exam</option>
+            <option>NEET</option>
+            <option>JEE</option> 
+            <option>MHT-CET</option>                  
+            </select>
+          </div>
+</div>
+
+<div class="btn-group btn-group-inline">
+<div class="form-group">
+            <!-- <label for="exampleFormControlSelect1">Answer</label> -->
+            <select class="form-control" name="answer" id="exampleFormControlSelect1">
+            <option selected disabled>Select Standard</option>
+            <option>XI</option>
+            <option>XII</option>                 
+            </select>
+          </div>
+</div>
+
+<div class="btn-group btn-group-inline">
+          <div class="form-group">
+            <!-- <label for="exampleFormControlSelect1">Answer</label> -->
+            <select class="form-control" name="answer" id="exampleFormControlSelect1">
+            <option selected disabled>Select Subject</option>
+            <option>Easy</option>
+            <option>Hard</option>                 
+            </select>
+          </div>
+</div>
+
+<div class="btn-group btn-group-inline">
+          <div class="form-group">
+            <!-- <label for="exampleFormControlSelect1">Answer</label> -->
+            <select class="form-control" name="answer" id="exampleFormControlSelect1">
+            <option selected disabled>Select Level</option>
+            <option>Easy</option>
+            <option>Hard</option>                 
+            </select>
+          </div>
+</div>
+  </div>  
+</div>
+<hr>
+<!-- <div class="row"> -->
+<div class="row">
+
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><strong>QUESTION</strong></h5>
+        <div class="form-group">
+                <!-- <strong>Question:</strong> -->
+                <input type="file" name="question" class="form-control-file" placeholder="Upload Question">
+               
+            </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><strong>ANSWER</strong></h5>
+        <div class="form-group">        
+            <select class="form-control" name="answer" id="exampleFormControlSelect1">
+            <option selected disabled>Select Answer</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+            <option>D</option>      
+            </select>
+        </div>     
+       
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><strong>HINT</strong></h5>
+        <div class="form-group">
+                <!-- <strong>Hint:</strong> -->
+                <input type="file" name="hint" class="form-control-file" placeholder="Hint">
+            </div>
+      </div>
+    </div>          
+  </div>
+  <hr>
     
-     <div class="row">
+</div>  
+
+<div class= "text-center">
+    <button type="submit" class="btn btn-primary m-2">Upload Question</button>   
+    </div>
+
+
+<!-- next -->
+<!-- <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
 
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Question:</strong>
-                <input type="file" name="question" class="form-control" placeholder="Upload Question">
+                <input type="file" name="question" class="form-control-file" placeholder="Upload Question">
             </div>
         </div>
 
         <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
-            <label for="exampleFormControlSelect1">Answer</label>
+        <strong>Answer:</strong>
             <select class="form-control" name="answer" id="exampleFormControlSelect1">
             <option selected disabled>Select Answer</option>
             <option>A</option>
@@ -164,33 +184,24 @@
         </div>            
         </div>
 
-        <!-- <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Detail:</strong>
-                <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail"></textarea>
-            </div>
-        </div> -->
+       
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Hint:</strong>
-                <input type="file" name="hint" class="form-control" placeholder="Hint">
+                <input type="file" name="hint" class="form-control-file" placeholder="Hint">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Upload</button>
+                <button type="submit" class="btn btn-primary m-2">Upload Question</button>
         </div>
-    </div>
-     
+      </div>
+
+</div> -->
 </form>
 
-
-   
-  </div>
-    
 </div>
-
-
-
+</div>
+<!-- //----------------------------- -->
     
   </div>
 </div></div>
